f86867460b65c3cc4b9e5569a32087ea2c6e29e4
Travis <tscheponik@gmail.com>
2015-02-01 17:43:14 -0500

    finished create/modify user

--- fanmobi/dao/user.py
+++ fanmobi/dao/user.py
@@ -1,4 +1,5 @@
-from .sqlutils import get_connection, Constants, build_where, hash_password
+from collections import OrderedDict
+from .sqlutils import get_connection, Constants, build_where, hash_password,build_set
 
 
 class UserDAO:
@@ -19,22 +20,31 @@ class UserDAO:
         criteria = {}
         if email_address:
             criteria['username'] = email_address
-        if facebook_id:
+        elif facebook_id:
             criteria['facebook_id'] = facebook_id
-        if twitter_id:
+        elif twitter_id:
             criteria['twitter_id'] = twitter_id
-        if user_id:
+        elif user_id:
             criteria['id'] = user_id
         if len(criteria) > 0:
             with conn.cursor(try_plain_query=True) as cursor:
                 cursor.execute("SELECT COUNT(1), ID FROM " + Constants.SCHEMA_NAME.value+"."+Constants.USERS.value
                                + build_where(criteria), criteria.values())
                 for row in cursor:
+                    print(row)
                     if row[0] > 0:
                             return {"exists": True, "id": row[1]}
         return {"exists": False, "id": -1}
 
-    def insert(self, email_address=None, password=None, facebook_id=None, twitter_id=None):
+    def upsert(self, email_address=None, password=None, facebook_id=None, twitter_id=None):
+        """
+          Updates or inserts a new record depending on if the user was found
+        :param email_address:
+        :param password:
+        :param facebook_id:
+        :param twitter_id:
+        :return: The user ID affected by this operation
+        """
         conn = get_connection()
         with conn.cursor(try_plain_query=True) as cursor:
             existence_check = self.user_exists(email_address, facebook_id, twitter_id)
@@ -47,12 +57,21 @@ class UserDAO:
                 user_id = cursor.lastrowid
             else:
                 user_id = existence_check['id']
+                criteria = OrderedDict()
+                if email_address:
+                    criteria['username'] = email_address
+                if facebook_id:
+                    criteria['facebook_id'] = facebook_id
+                if twitter_id:
+                    criteria['twitter_id'] = twitter_id
+                set_clause = build_set(criteria)
+                criteria['ID'] = user_id
+                cursor.execute("UPDATE " + Constants.SCHEMA_NAME.value+"."+Constants.USERS.value
+                               + set_clause + " " + build_where({"ID": None}),
+                               criteria.values())
         return user_id
 
     def delete(self):
-        pass
-
-    def update(self):
         pass
 
     def find(self, criteria=None):
@@ -70,9 +89,9 @@ class UserDAO:
 def main():
     dao = UserDAO()
     dict = {"username": 'test@test.com'}
-    dao.user_exists(email_address='test@test.com')
+    #dao.user_exists(email_address='test@test.com')
    # dao.find(dict)
-   # dao.insert('t@t.com', 'password', 'uuu', 'aaa')
+    dao.upsert(email_address='t@t.com', password=None,facebook_id='UPDATED', twitter_id='aaa')
 
 if __name__ == '__main__':
     main()
--- fanmobi/services/service.py
+++ fanmobi/services/service.py
@@ -5,6 +5,9 @@ from fanmobi.dao import user
 
 
 class UserHandler(tornado.web.RequestHandler):
+    """
+       Responsible for handling the User API calls
+    """
 
     def put(self):
         if not self.request.body:
@@ -12,7 +15,7 @@ class UserHandler(tornado.web.RequestHandler):
         else:
             data = escape.json_decode(self.request.body)
         dao = user.UserDAO()
-        generated_id = dao.insert(email_address=data['email'], password=data['password'],
+        generated_id = dao.upsert(email_address=data['email'], password=data['password'],
                                   facebook_id=data['facebook-id'], twitter_id=data['twitter-id'])
         self.write("{ \"user_id\": \"" + str(generated_id) + "\" }")
 
